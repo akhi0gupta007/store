51d34e6a12e9b48115ea0c592b97b0f326267380
Akhilesh <adc@mail.akhilesh.com>
2013-04-22 15:20:01 +0530

    DAO

--- src/main/java/com/akhi/store/util/HibernateUtil.java
+++ src/main/java/com/akhi/store/util/HibernateUtil.java
@@ -5,30 +5,50 @@ import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import com.akhi.store.general.User;
+
 @Repository
-public class HibernateUtil {
+public class HibernateUtil implements DaoUtils
+    {
 
-	private SessionFactory sessionFactory;
+    private SessionFactory sessionFactory;
 
-	@Autowired
-	public HibernateUtil(SessionFactory sessionFactory) {
-		super();
-		this.sessionFactory = sessionFactory;
-	
-		
+    @Autowired
+    public HibernateUtil(SessionFactory sessionFactory)
+	{
+	this.sessionFactory = sessionFactory;
+	System.out.println("Session Factory: " + sessionFactory);
+	//System.out.println("Session : "+ getSession());
 	}
 
-	public SessionFactory getSessionFactory() {
-		return sessionFactory;
+    public SessionFactory getSessionFactory()
+	{
+	return sessionFactory;
 	}
 
-	@Override
-	public String toString() {
-		return "HibernateUtil [sessionFactory=" + sessionFactory + "]";
+    @Override
+    public String toString()
+	{
+	return "HibernateUtil [sessionFactory=" + sessionFactory + "]";
 	}
 
-	public Session getSession() {
-		return sessionFactory.getCurrentSession();
+    @Override
+    public Session getSession()
+	{
+	return sessionFactory.getCurrentSession();
 	}
 
-}
+    @Override
+    public void insertUser( User user )
+	{
+	getSession().save(user);
+
+	}
+
+    @Override
+    public User getUser( long id )
+	{
+	return (User)getSession().get(User.class, id);
+	}
+
+    }
