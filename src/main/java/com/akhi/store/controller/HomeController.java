0fd1bf0358f38a7834e2cf08b1bcd1556f2ca18a
akhi0gupta007 <akhi0gupta007@gmail.com>
2013-04-29 15:42:02 +0530

    service changes

--- src/main/java/com/akhi/store/controller/HomeController.java
+++ src/main/java/com/akhi/store/controller/HomeController.java
@@ -1,10 +1,5 @@
 package com.akhi.store.controller;
 
-/**
- * Hello world!
- */
-import javax.servlet.http.HttpSession;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
--- src/main/java/com/akhi/store/service/UserServiceImpl.java
+++ src/main/java/com/akhi/store/service/UserServiceImpl.java
@@ -9,7 +9,9 @@
 package com.akhi.store.service;
 
 import org.apache.log4j.Logger;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.criterion.Restrictions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Required;
 import org.springframework.stereotype.Service;
@@ -42,8 +44,8 @@ public class UserServiceImpl implements UserService
     {
 
     private SessionFactory sessionFactory;
-    
-    private static Logger log = Logger.getLogger(UserServiceImpl.class);
+
+    private static Logger  log = Logger.getLogger(UserServiceImpl.class);
 
     @Override
     public void addUser( User user )
@@ -60,7 +62,30 @@ public class UserServiceImpl implements UserService
     @Override
     public User autheticate( String userId, String password )
 	{
-	return null;
+
+	User user = null;
+
+	Session session = sessionFactory.getCurrentSession();
+
+	if (session != null)
+	    {
+	    user = (User) session.createCriteria(User.class).add(Restrictions.eq("userId",
+										 userId)).add(Restrictions.eq("password",
+													      password)).setMaxResults(1).uniqueResult();
+	    if (user != null)
+		{
+		log.info("User found" + user);
+		}
+	    else
+		{
+		log.warn("No Idea abt user" + userId);
+		}
+	    }
+	else
+	    {
+	    log.fatal("No Sesssion Found");
+	    }
+	return user;
 
 	}
 
@@ -68,7 +93,7 @@ public class UserServiceImpl implements UserService
 	{
 	return sessionFactory;
 	}
-    
+
     @Autowired
     @Required
     public void setSessionFactory( SessionFactory sessionFactory )
