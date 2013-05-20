454c1f4a9dbbb70fdbf1776551a078106f733601
akhi0gupta007 <akhi0gupta007@gmail.com>
2013-05-20 17:58:29 +0530

    test cases

---
+++ src/test/java/com/akhi/store/test/UserDaoTest.java
@@ -0,0 +1,68 @@
+package com.akhi.store.test;
+
+import org.apache.log4j.Logger;
+import org.hibernate.Criteria;
+import org.junit.After;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
+import org.springframework.test.context.transaction.TransactionConfiguration;
+import org.springframework.test.context.transaction.TransactionalTestExecutionListener;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.akhi.store.dao.UserDao;
+import com.akhi.store.general.Profile;
+import com.akhi.store.general.User;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations =
+    { "classpath:storeBean.xml" })
+@TransactionConfiguration(transactionManager = "txMgr", defaultRollback = true)
+@TestExecutionListeners(
+    { DependencyInjectionTestExecutionListener.class, TransactionalTestExecutionListener.class })
+@Transactional
+public class UserDaoTest
+    {
+
+    @Autowired
+    private UserDao      dao;
+
+    public static Logger log = Logger.getLogger(UserDaoTest.class);
+
+    @After
+    public void cleanup()
+	{
+	//deleteFromTables(jdbcTemplate, "spitter");
+	}
+
+    @Test
+    public void shouldCreateRowsAndSetIds()
+	{
+	User user = new User();
+	user.setEmailId("akhi0gupta007@gmail.com");
+	user.setUserId("akhi");
+	user.setPassword("password");
+	Profile profile = new Profile();
+	profile.setPhone("9718304337");
+	profile.setUser(user);
+	user.setProfile(profile);
+	log.info("DAO: " + dao.makePersistent(user));
+	log.info(user.getProfile() + "  " + profile.getUser());
+	
+	log.info("Searching by User and password");
+	assertEquals("Found the same object",user, dao.findByIdAndPassword("akhi","password"));
+
+	}
+
+    @Test
+    public void shouldBeAbleToFindByCriteria()
+	{
+	
+	//log.info("DAO    2   "+dao);
+	}
+    }
